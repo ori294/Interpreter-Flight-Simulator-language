beacea6c7a62ac9ebda726afbc826850e1c06c87
Ori Levy <ori294@gmail.com>
2019-12-17 19:30:20 +0200

    add a short description for what you've done

---
+++ ex3/Commands/VarAssignCommand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ori294 on 12/17/19.
+//
+
+#include "VarAssignCommand.h"
---
+++ ex3/Commands/VarAssignCommand.h
@@ -0,0 +1,12 @@
+//
+// Created by ori294 on 12/17/19.
+//
+
+#ifndef EX3_COMMANDS_VARASSIGNCOMMAND_H_
+#define EX3_COMMANDS_VARASSIGNCOMMAND_H_
+
+class VarAssignCommand {
+
+};
+
+#endif //EX3_COMMANDS_VARASSIGNCOMMAND_H_
